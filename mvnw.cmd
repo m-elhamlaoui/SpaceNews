e59da795e984dda0440086c26abebee8006d6181
fatima zhr mlk <117781473+fatimamlk@users.noreply.github.com>
2024-06-27 17:38:21 +0100

    Delete mvnw.cmd

--- mvnw.cmd
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-set MAVEN_VER=3.8.6
-IF NOT EXIST ".mvn" (
-  mkdir ".mvn\wrapper"
-  curl -o ".mvn\wrapper\maven-wrapper.jar" "https://repo1.maven.org/maven2/io/takari/maven-wrapper/0.5.6/maven-wrapper-0.5.6.jar"
-  echo distributionUrl=https://repo1.maven.org/maven2/org/apache/maven/apache-maven/%MAVEN_VER%/apache-maven-%MAVEN_VER%-bin.zip > .mvn\wrapper\maven-wrapper.properties
-)
-java -jar .mvn\wrapper\maven-wrapper.jar %*
